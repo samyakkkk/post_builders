fc561b6c5345f6b108b089de841b58144762d295
Samyak Jain <56000318+samyakkkk@users.noreply.github.com>
2023-06-08 22:48:11 +0530

    Initial Commit

---
+++ example/post_builders_example.dart
@@ -0,0 +1,6 @@
+import 'package:post_builders/post_builders.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/post_builders_test.dart
@@ -0,0 +1,16 @@
+import 'package:post_builders/post_builders.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
